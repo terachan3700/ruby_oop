5617609384957f6cdfbc292f59e6e7932740adaa
Junya Terada <chapico01@gmail.com>
2021-04-20 08:28:31 +0900

    create wheel class

--- sample.rb
+++ sample.rb
@@ -1,10 +1,9 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog       = cog
-    @rim       = rim
-    @tire       = tire
+    @wheel = wheel
   end
 
   def ratio
@@ -12,9 +11,30 @@ class Gear
   end
 
   def gear_inchecs
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
   end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inchecs
-puts Gear.new(52, 11, 24, 1.25).gear_inchecs
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inchecs
+
+puts Gear.new(52, 11).ratio
